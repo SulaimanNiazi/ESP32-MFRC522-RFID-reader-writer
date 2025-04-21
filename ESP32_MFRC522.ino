fd70c1293fa8c88e4af4ef7cdd7c384efea71ae0
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-04-21 11:40:52 +0500

    Improve block address storage to allow more than 1 digit values

--- ESP32_MFRC522.ino
+++ ESP32_MFRC522.ino
@@ -18,41 +18,45 @@ void setup() {
 }
 
 void loop(){
-  MFRC522::MIFARE_Key key;                                    // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
-  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
-
+  Serial.print("**Select Mode**\n1. Change UID Mode\n2. Read Block Mode\n3. Write Block Mode\nChoice: ");
+  
+  byte buffer[34] = "", len = 0, block_size = 20, choice = readUART()[0];
   MFRC522::StatusCode status;
   String string = "";
-
-  Serial.print("**Select Mode**\n1. Change UID Mode\n2. Read Block Mode\n3. Write Block Mode\nChoice: ");
-  byte buffer[34] = "", len = 0, block_size = 20, choice = readUART()[0];
+  MFRC522::MIFARE_Key key;
   
+  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;         // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+
   switch(choice){
     case '1':
-      Serial.println("Change UID Mode Selected");
+      Serial.println("Change UID Mode Selected\nEnter new UID value: ");
+      string = readUART();
+      
       while(1){
         awaitCardDetection();
+
       }
       break;
     case '2':
-      Serial.println("Read Block Mode Selected");
-      Serial.print("Enter Block Number: ");
-      choice = readUART()[0] - '0';
+      Serial.print("Read Block Mode Selected\nEnter Block Number: ");
+      choice = readUART().toInt();
       Serial.print("Block ");
       Serial.print(choice);
       Serial.print(" Selected.\nEnter the number of characters to read: ");
-      string = readUART();
-      len = string.toInt();
+      len = readUART().toInt();
       Serial.print(len);
       Serial.println(" characters will be read.\nBring a card near to read.");
+      
       while(1){
         awaitCardDetection();
+        
         status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_B, choice, &key, &(mfrc522.uid));
         if (status != MFRC522::STATUS_OK) {
           Serial.print(F("Authentication failed: "));
           Serial.println(mfrc522.GetStatusCodeName(status));
         }else{
           status = mfrc522.MIFARE_Read(choice, buffer, &block_size);
+          
           if (status != MFRC522::STATUS_OK) {
             Serial.print(F("Reading failed: "));
             Serial.println(mfrc522.GetStatusCodeName(status));
@@ -66,25 +70,27 @@ void loop(){
       }
       break;
     case '3':
-      Serial.println("Write Block Mode Selected");
-      Serial.print("Enter Block Number: ");
-      choice = readUART()[0] - '0';
+      Serial.print("Write Block Mode Selected\nEnter Block Number: ");
+      choice = readUART().toInt();
       Serial.print("Block ");
       Serial.print(choice);
-      Serial.println(" Selected.");
-      Serial.println("Enter block data:");
+      Serial.println(" Selected.\nEnter block data:");
       string = readUART();
       string.getBytes(buffer, block_size);
       for (int i = string.length(); i < block_size; i++) buffer[i] = ' '; // pad with spaces
       Serial.println("Block data saved.\nBring a card near to write.");
+      
       while(1){
         awaitCardDetection();
+        
         status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_B, choice, &key, &(mfrc522.uid));
+        
         if (status != MFRC522::STATUS_OK) {
           Serial.print(F("PCD_Authenticate() failed: "));
           Serial.println(mfrc522.GetStatusCodeName(status));
         }else{
           status = mfrc522.MIFARE_Write(choice, buffer, 16);
+          
           if (status != MFRC522::STATUS_OK) {
             Serial.print(F("MIFARE_Write() failed: "));
             Serial.println(mfrc522.GetStatusCodeName(status));
@@ -138,6 +144,7 @@ String readUART() {
 void awaitCardDetection(){
   mfrc522.PICC_HaltA();                                   // Halt the previous card, ending the communication session
   mfrc522.PCD_StopCrypto1();                              // Stop the encryption on the reader, resetting its state
+  
   while(1){
     if (mfrc522.PICC_IsNewCardPresent()){                 // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
       if (mfrc522.PICC_ReadCardSerial()){                 // Select one of the cards
